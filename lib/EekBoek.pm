1648914f6c230a8b1b25d90014918440f25198c7
Johan Vromans <jv@cpan.org>
2006-03-24 14:07:57 +0000

    Import of JV/EekBoek-0.51 from CPAN.
    gitpan-cpan-distribution: EekBoek
    gitpan-cpan-version:      0.51
    gitpan-cpan-path:         JV/EekBoek-0.51.tar.gz
    gitpan-cpan-author:       JV
    gitpan-cpan-maturity:     released

--- lib/EekBoek.pm
+++ lib/EekBoek.pm
@@ -1,6 +1,6 @@
 package EekBoek;
 
-$VERSION = '0.50';
+$VERSION = '0.51';
 
 # NOTE: This is a documentation-only module.
 
@@ -55,4 +55,4 @@ GNU General Public License or the Artistic License for more details.
 
 1;
 
-# $Id: EekBoek.pm,v 1.25 2006/03/16 15:16:43 jv Exp $
+# $Id: EekBoek.pm,v 1.27 2006/03/24 13:53:07 jv Exp $
